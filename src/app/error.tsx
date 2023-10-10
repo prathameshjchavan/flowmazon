bb41f6425cedf34cb0bff17fdee878b1186e982d
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-10 19:17:32 +0530

    added error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+type Props = {};
+
+const error = (props: Props) => {
+  return <div>Something went wrong. Plese refresh the page.</div>;
+};
+
+export default error;
