34bfd71e56ed1693083676f1a32d4cb36dddf10a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-14 11:47:27 +0530

    loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const LoadingPage = (props: Props) => {
+  return <span className="loading-ls loading loading-dots m-auto block" />;
+};
+
+export default LoadingPage;
