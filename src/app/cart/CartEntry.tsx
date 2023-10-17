7cf0188de278de0f19c2db3e936a0ff11517d553
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-17 20:41:31 +0530

    cart product info

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { CartItemWithProducts } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/format";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -19,6 +21,29 @@ const CartEntry = ({ cartItem: { product, quantity } }: Props) => {
           height={200}
           className="rounded-lg"
         />
+        <div>
+          <Link href={`/products/${product.id}`} className="font-bold">
+            {product.name}
+          </Link>
+          <div>Price: {formatPrice(product.price)}</div>
+          <div className="my-1 flex items-center gap-2">
+            Quantity:{" "}
+            <select
+              name="quantity"
+              className="select select-bordered w-full max-w-[80px]"
+              defaultValue={quantity}
+            >
+              {new Array(99).fill(null).map((_, index) => (
+                <option key={index + 1} value={index + 1}>
+                  {index + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex items-center gap-2">
+            Total: {formatPrice(product.price * quantity)}
+          </div>
+        </div>
       </div>
       <div className="divider" />
     </div>
