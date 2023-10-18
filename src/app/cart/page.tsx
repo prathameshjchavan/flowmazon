7d579cefdd6925eef4f3f69c13e19d4327258864
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-18 17:14:14 +0530

    empty cart ui

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@ import { getCart } from "@/lib/db/cart";
 import React from "react";
 import CartEntry from "./CartEntry";
 import { setProductQuantity } from "./action";
+import { formatPrice } from "@/lib/format";
 
 type Props = {};
 
@@ -22,6 +23,13 @@ const CartPage = async (props: Props) => {
           cartItem={cartItem}
         />
       ))}
+      {!cart?.items.length && <p>Your cart is empty.</p>}
+      <div className="flex flex-col items-end sm:items-center">
+        <p className="mb-3 font-bold">
+          Total: {formatPrice(cart?.subtotal || 0)}
+        </p>
+        <button className="btn btn-primary sm:w-[200px]">Checkout</button>
+      </div>
     </div>
   );
 };
