75cccc9614828eab4b77a3c73254bc8c81ca6eb8
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-21 11:41:04 +0530

    added pagination in search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,4 @@
+import PaginationBar from "@/components/PaginationBar";
 import ProductCard from "@/components/ProductCard";
 import { prisma } from "@/lib/db/prisma";
 import { Metadata } from "next";
@@ -6,6 +7,7 @@ import React from "react";
 interface Props {
   searchParams: {
     query: string;
+    page: string;
   };
 }
 
@@ -15,8 +17,10 @@ export function generateMetadata({ searchParams: { query } }: Props): Metadata {
   };
 }
 
-const SearchPage = async ({ searchParams: { query } }: Props) => {
-  const products = await prisma.product.findMany({
+const SearchPage = async ({ searchParams: { query, page = "1" } }: Props) => {
+  const currentPage = Number(page);
+  const pageSize = 6;
+  const totalItemCount = await prisma.product.count({
     where: {
       OR: [
         { name: { contains: query, mode: "insensitive" } },
@@ -25,15 +29,32 @@ const SearchPage = async ({ searchParams: { query } }: Props) => {
     },
     orderBy: { id: "desc" },
   });
+  const totalPages = Math.ceil(totalItemCount / pageSize);
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    skip: (currentPage - 1) * pageSize,
+    take: pageSize,
+    orderBy: { id: "desc" },
+  });
 
-  if (products.length === 0)
+  if (totalItemCount === 0)
     return <div className="text-center">No products found</div>;
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="flex flex-col items-center">
+      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      {totalPages > 1 && (
+        <PaginationBar currentPage={currentPage} totalPages={totalPages} />
+      )}
     </div>
   );
 };
