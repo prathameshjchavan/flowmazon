bb748c28bdd610e9b6b439fb10656899ad9494c3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-21 11:40:45 +0530

    add page query param from PaginationBar

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import React, { Fragment } from "react";
 
 interface Props {
@@ -7,21 +9,29 @@ interface Props {
 }
 
 const PaginationBar = ({ currentPage, totalPages }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
   const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
   const numberedPageItems: JSX.Element[] = [];
 
+  const addPageQueryParam = (page: number) => {
+    const query = new URLSearchParams(Array.from(searchParams.entries()));
+    query.set("page", page.toString());
+    router.push(`${pathname}?${query}`);
+  };
+
   for (let page = minPage; page < maxPage; page++) {
     numberedPageItems.push(
-      <Link
-        key={page}
-        href={`?page=${page}`}
+      <button
+        onClick={() => addPageQueryParam(page)}
         className={`btn join-item ${
           currentPage === page ? "btn-active pointer-events-none" : ""
         }`}
       >
         {page}
-      </Link>,
+      </button>,
     );
   }
 
@@ -30,17 +40,23 @@ const PaginationBar = ({ currentPage, totalPages }: Props) => {
       <div className="join hidden sm:block">{numberedPageItems}</div>
       <div className="join sm:hidden">
         {currentPage > 1 && (
-          <Link href={`?page=${currentPage - 1}`} className="btn join-item">
+          <button
+            onClick={() => addPageQueryParam(currentPage - 1)}
+            className="btn join-item"
+          >
             «
-          </Link>
+          </button>
         )}
         <button className="btn join-item pointer-events-none">
           Page {currentPage}
         </button>
         {currentPage < totalPages && (
-          <Link href={`?page=${currentPage + 1}`} className="btn join-item">
+          <button
+            onClick={() => addPageQueryParam(currentPage - 1)}
+            className="btn join-item"
+          >
             »
-          </Link>
+          </button>
         )}
       </div>
     </Fragment>
